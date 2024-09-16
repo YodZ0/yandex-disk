00405128a1ca99be5fba0b1c3a30717154989fce
Timur YodZ0 <tima-mustafa@mail.ru>
2024-09-16 14:03:41 +0600

    YC gets cookies

--- backend/src/api/api_v1/deps.py
+++ backend/src/api/api_v1/deps.py
@@ -1,9 +1,12 @@
+from fastapi import Request, HTTPException
+
 from src.core.config import settings
 from src.services import YandexClient
 
 
-async def get_yandex_client(token: str) -> YandexClient:
-    return YandexClient(
-        base_url=settings.yandex_client.base_url,
-        oauth_token=token,
-    )
+async def get_yandex_client(request: Request) -> YandexClient:
+    token = request.cookies.get("oauth_token")
+    if not token:
+        raise HTTPException(status_code=401, detail="Token not found in cookies")
+
+    return YandexClient(base_url=settings.yandex_client.base_url, oauth_token=token)
