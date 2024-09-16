7c0cab91a9230a63a0d6915f4574580e32385b6e
Timur YodZ0 <tima-mustafa@mail.ru>
2024-09-16 13:12:10 +0600

    Add token validation

--- backend/src/api/api_v1/auth.py
+++ backend/src/api/api_v1/auth.py
@@ -1,10 +1,9 @@
 from aiohttp import ClientSession
 
-from fastapi import APIRouter, Response
+from fastapi import APIRouter
 from pydantic import BaseModel
 
 from src.core.config import settings
-
 
 router = APIRouter(prefix=settings.api.v1.auth, tags=["Auth"])
 
@@ -25,21 +24,3 @@ async def validate_token(token: OAuthToken):
         ) as response:
             res = await response.json()
             return res
-
-
-@router.post("/set")
-async def set_cookie(
-    token: OAuthToken,
-    response: Response,
-):
-    try:
-        response.set_cookie(
-            key="oauth_token",
-            value=token.token,
-            samesite="none",
-            secure=True,
-        )
-        return {"status": "200", "message": "success"}
-    except Exception as e:
-        print(e)
-        return {"status": "500", "message": "error"}
