c39ed7e302a10d192bd7d66211e136c63a691e86
Timur YodZ0 <tima-mustafa@mail.ru>
2024-09-17 03:38:10 +0600

    Modify css + add button customization

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,7 +1,29 @@
 import './Button.css';
 
-export default function Button({ title, onClick, type}) {
+export default function Button({
+    title = undefined,
+    onClick,
+    type = "button",
+    icon = undefined,
+    onFileChange = undefined,
+}) {
     return (
-        <button className='button' onClick={onClick} type={type}>{title}</button>
+        <button
+            className='button'
+            onClick={onClick}
+            type={type !== "file" ? type : undefined}
+        >
+            <div className='button-children'>
+                {icon}
+                {title}
+            </div>
+            {type === "file" && (
+                <input
+                    type="file"
+                    className="file-input"
+                    onChange={onFileChange}
+                />
+            )}
+        </button>
     )
 }
