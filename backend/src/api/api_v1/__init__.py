ab771bb4b2519d70a6f9d9006be4c031ed85d9b3
Timur YodZ0 <tima-mustafa@mail.ru>
2024-09-18 19:46:21 +0600

    Fix invalid imports

--- backend/src/api/api_v1/__init__.py
+++ backend/src/api/api_v1/__init__.py
@@ -4,7 +4,6 @@ from src.core.config import settings
 
 from .auth import router as auth_router
 from .disk import router as disk_router
-from .test import router as test_router
 
 # Main API V1 router
 router = APIRouter(
@@ -14,4 +13,3 @@ router = APIRouter(
 # Add api_v1 routers here
 router.include_router(router=auth_router)
 router.include_router(router=disk_router)
-router.include_router(router=test_router)
